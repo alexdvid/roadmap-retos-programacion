dd95fc57a164d9b77b943a642c70a2e1a44bd354
alexdvid <dvd.c.a93@gmail.com>
2024-10-07 23:30:20 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/alexdvid.py
@@ -0,0 +1,28 @@
+"""
+Leguaje de programación escogido: Python
+
+Sitio oficial de descarga: https://www.python.org/
+
+versión: 3.13
+
+"""
+
+
+
+# Apartado de variables
+
+medio_valor = 0,5 #tipo de variable float
+valor = 1 #tipo de variable int
+ruta = "./cocina/heladera/Freezer" #tipo de variable string
+Permitido = True #tipo de variable boolean
+No_Permitido = False #tipo de variable boolean
+
+'''
+Aunque Python no tiene un mecanismo nativo para declarar constantes, 
+la convención de usar letras mayúsculas para definir variables que deberían tratarse 
+como constantes es ampliamente aceptada por la comunidad de Python. 
+Esta es una convención, no una regla forzada por el lenguaje.
+'''
+HORAS_DEL_DIA = 24 #Tipo de Variable constante por convención
+
+print ("Hola, Python!")
